a28dfc40950088292d7cbe484f4e8808e9806fc9
Stetsyuk Vladislav <admin@kustosoft.com>
2023-11-13 05:15:26 +0300

    Add index link to ui

--- services/AUTOMATIC1111/info.py
+++ services/AUTOMATIC1111/info.py
@@ -7,7 +7,7 @@ file.write_text(
   .replace('    return demo', """
     with demo:
         gr.Markdown(
-          'Created by [AUTOMATIC1111 / stable-diffusion-webui-docker](https://github.com/AbdBarho/stable-diffusion-webui-docker/)'
+          'Created by [AUTOMATIC1111 / stable-diffusion-webui-docker](https://github.com/AbdBarho/stable-diffusion-webui-docker/)\\n\\n[Go to index](/)'
         )
     return demo
 """, 1)
